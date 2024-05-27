d015d5fc3dc391edb61592133eace130ce250f69
sam <samuelmebersole@gmail.com>
2024-05-27 17:39:24 -0400

    More info on About page

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,51 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Markdown from 'react-markdown';
 
-/*
-import Section from '../Section';
-
-const SECTIONS = [
-  {
-    title: 'Experience',
-    body: [
-      {
-        title: 'Columbia Physics Lab Course Assistant',
-        body: `- Design, prototype and test equipment for experiments.
-- Troubleshoot lab equipment and perform repairs.`,
-      },
-      {
-        title: 'TOM Makeathon Maker',
-        body: `- Interview users to aid research.
+const markdown = `
+## Experience
+### Columbia Physics Lab Course Assistant
+- Design, prototype and test equipment for experiments.
+- Troubleshoot lab equipment and perform repairs.
+### TOM Makeathon Maker
+- Interview users to aid research.
 - Ideate solutions and draw them.
-- Create the cardboard and aluminum protoypes.`,
-      },
-    ],
-  },
-  {
-    title: 'Skills',
-    body: `- Solidworks.
+- Create the cardboard and aluminum protoypes.
+## Skills
+- Solidworks.
 - Python.
 - Matlab.
 - 3D printing.
 - Laser cutting.
 - Laser engraving.
-- Machining.`,
-  },
-  {
-    title: 'Courses',
-    body: `- Computer Graphics & Design.
+- Machining.
+## Courses
+- Computer Graphics & Design.
 - Heat Transfer.
 - Computer Aided Design.
 - Mechanics of Solids (Focusing on FEA).
-- Human-Centered Design.`,
-  },
-];
-*/
+- Human-Centered Design.`;
 
 export default function About() {
   return (
     <>
       <h1>About</h1>
-      {/* SECTIONS.map((section) => <Section key={section.title} level={2} section={section} />) */}
+      <p>
+        Achilles is a product engineer committed to human centered design. He seeks to
+        improve his design skills with socially impactful projects like
+        {' '}
+        <Link to="/projects/thimble/tst">the Thimble.</Link>
+        {' '}
+        He is a graduate of Columbia University with a B.S. in mechanical engineering and has
+        worked with various educational spaces at Columbia to design and develop educational
+        devices with them.
+      </p>
+      <Markdown>{markdown}</Markdown>
     </>
   );
 }
