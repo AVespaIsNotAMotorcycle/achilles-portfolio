c7f6562e7f912d3938887f0c83f7e145666c9f05
sam <samuelmebersole@gmail.com>
2024-05-30 14:16:02 -0400

    Removed duplicate Accordion code

--- src/Projects/DeckBox/DeckBox.jsx
+++ src/Projects/DeckBox/DeckBox.jsx
@@ -1,9 +1,7 @@
-import React, { useState } from 'react';
-import propTypes from 'prop-types';
+import React from 'react';
 
 import Section from '../../Section';
-
-import './accordion.css';
+import Accordion from '../../Accordion';
 
 import COMPETITOR_BOX_ONE from './research/competitor-1.png';
 import COMPETITOR_BOX_TWO from './research/competitor-2.png';
@@ -29,27 +27,6 @@ export const SECTIONS = {
       CAD: { name: 'CAD' },
     },
   },
-};
-
-function Accordion({ images }) {
-  const [selected, setSelected] = useState(0);
-  return (
-    <ol className="accordion">
-      {images.map(({ source, alt }, index) => (
-        <li key={source} className={index === selected ? 'selected' : ''}>
-          <button type="button" onClick={() => { setSelected(index); }}>
-            <img src={source} alt={alt} />
-          </button>
-        </li>
-      ))}
-    </ol>
-  );
-}
-Accordion.propTypes = {
-  images: propTypes.arrayOf(propTypes.shape({
-    source: propTypes.string,
-    alt: propTypes.string,
-  })).isRequired,
 };
 
 export default function DeckBox() {
